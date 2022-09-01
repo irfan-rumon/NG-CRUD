56bb472ab25759712095fcf5564d4a88e0b126d9
irfan-rumon <irfannisho8571@gmail.com>
2022-09-01 17:46:20 +0600

    Add home component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FrameworkComponent } from './framework/framework.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent}, 
+  { path: '', component: HomeComponent}, 
+  { path: 'list', component: FrameworkComponent}, 
  
 
 ];
