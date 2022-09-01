82e036d9ba505d4e912928e8a3c0a1bd61c3d572
irfan-rumon <irfannisho8571@gmail.com>
2022-09-01 14:46:59 +0600

    Add CRUD functionalities at framework component

--- frontend/src/app/framework/framework.component.ts
+++ frontend/src/app/framework/framework.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Framework } from '../framework';
+import { ApiService } from './api.service';
+import {Router} from "@angular/router"
 
 @Component({
   selector: 'app-framework',
@@ -14,9 +16,37 @@ export class FrameworkComponent implements OnInit {
   @Input() isAdd: boolean = false;
   currentEditableFr: Framework;
 
-  constructor() { }
+  constructor(private api: ApiService, private router: Router) { }
 
   ngOnInit(): void {
+    this.api.getFrameworks().subscribe((frameworks) => (this.frameworks = frameworks));
+    console.log(this.frameworks);
+  }
+
+  addFramework(framework: Framework) {
+    console.log('add framework!!');
+    this.api.addFramework(framework).subscribe((framework) => this.frameworks.push(framework));
+    this.router.navigate(['/list']);
+  }
+
+  deleteFramework(framework: Framework) {
+    this.api
+      .deleteFramework(framework)
+      .subscribe(
+        () => (this.frameworks = this.frameworks.filter((t) => t.id !== framework.id))
+      );
+      this.isEdit = false; this.isList=true; this.isAdd=false; this.isHome=false;
+      this.router.navigate(['/list']);
+  }
+
+  editFramework(framework: Framework) {
+    console.log("edite entered------>!!");
+    this.api.editFramework(framework).subscribe(
+      (res) => console.log(res)
+    );
+    this.isEdit = false;
+    console.log(this.frameworks);
+    this.router.navigate(['/list']);
   }
 
 }
