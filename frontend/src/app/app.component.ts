5f7f781531a870920995ca4877f23590dd7a3caa
irfan-rumon <irfannisho8571@gmail.com>
2022-09-01 13:53:28 +0600

    Add component folders

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { FrameworkComponent } from './framework/framework.component';
+import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
